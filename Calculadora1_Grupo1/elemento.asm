5604857784f3575765d297d0c0e883b92fa73f76
Rebecca Medina <rebemedi99@gmail.com>
2020-11-19 00:23:27 -0600

    otro

--- Calculadora1_Grupo1/elemento.asm
+++ Calculadora1_Grupo1/elemento.asm
@@ -7,98 +7,51 @@
 INCLUDE c:\Irvine\Irvine32.inc
 INCLUDELIB c:\Irvine\Irvine32.lib
 
-org 100h
-use16
-start:
-;limpiar la pantalla
-mov	ah,00h
-mov	al,03h
-int	10h
-;Mensaje de bienvenida
-mov	ah,9h
-mov	dx,msg
-int 	21h
-;esperar numeros
-Esperar:
-mov	ah,00h
-int	16h
-;echo
-mov	ah, 0eh
-int	10h
-;operar conforme se presionan las teclas
-cmp	al,27   	;salir con q
-je	SalirEsc
-cmp	al,'+'		;detectar suma
-je	Suma
-cmp	al,'-'		;detectar resta
-je	Resta
-cmp	al,'*'		;detectar multiplicacion
-je	Mult
-cmp	al,'/'		;detectar division
-je	Divi
-cmp	al,13		;operar
-je	Operar
-;ver si es numero
-mov	cl,al
-push	cx		;guardar el numero a la pila
-sub	al, 40h		;ver si es numero
-cmp	al, 0
-jge	Error
+Menu: ;esta es una etiqueta se llama menu que sera donde mostraremos las opciones al usuario
+     mov ah,0
+     mov al,3h ;modo texto
+     int 10h ; interrupcion de video
 
-jmp	Esperar		;esperar siguiente numero o tecla
+     mov ax,0600h ;limpiar pantalla
+     mov bh,0fh ;0 color de fondo negro, f color de letra blanco
+     mov cx,0000h
+     mov dx,184Fh
+     int 10h
 
+     mov ah,02h
+     mov bh,00
+     mov dh,00
+     mov dl,00
+     int 10h
+     mov dx, mostrarMenu ;nombre del mensaje
+     mov ah,09 ; mov ah,09 sirve para presentar el msj en pantalla
+     int 21h ;interrupcion de video
 
+     mov ah,08 ;pausa y captura de datos 08 espera que el usuario presione una tecla
+     int 21h
 
-Suma:
-;acciones para sumar cosas
-pop 	cx
-add	al,cl
-int	21h
-jmp	Esperar
+     cmp al,49 ; ascii 49 = numero 1 compara lo que tiene el registro ah con el ascii 49 en el reg al
+     je op1 ; salto condicional jump equals opcion 1 saltar si es igual a la opcion 1
 
-Resta:
-;acciones para restar cosas
-pop	cx
-sub	al,cl
-jmp	 Esperar
+     cmp al,50 ;compara con opcion 2 apellido
+     je op2
 
-Mult:
-;acciones para multiplicar cosas
-pop	cx
-;mul   	al,cl 
-jmp	Esperar
+     cmp al,51 ;compara con opcion 3 carne
+     je op3
 
-Divi:
-;acciones para dividir cosas
-pop	cx
-;div	al,cl
-jmp 	Esperar
+     cmp al,52 ;compara con opcion 4 salir
+     je op4
 
-Operar:
-;acciones para operar
-pop	cx
-mov	ah,09h
-mov	dx,opact
-int	21h
-jmp	Esperar
-
-Error:
-mov	ah,09h
-mov	dx,error
-int 	21h
-jmp	Esperar
-
-SalirEsc:
-mov	ah,09h
-mov	dx,adios
-int 	21h
-ret
-
-num	db ?  
-opact	db "operacion realizada:",13,10,"$"
-msg	db "Bienvenido a la calculadora",13,10,"$"
-adios	db 13,10,"Adios :)",13,10,"$"
-error	db 13,13,"esto no es un numero",13,10,"$"
+     mostrarMenu db 'Universidad San Carlos de Guatemala',13,10 ; 13 es un 'enter' 10 un espacio
+                 db 'Facultad de Ingenieria',13,10
+                 db 'Escuela de Ciencias y Sistemas',13,10
+                 db 'Arquitectura de Computadoras y Ensambladores 1',13,10,13,10
+                 db '-.-.MENU.-.-',13,10,13,10
+                 db '1. Ver Nombre',13,10
+                 db '2. Ver Apellido',13,10
+                 db '3. Ver Carne',13,10
+                 db '4. Salir',13,10,13,10
+                 db 'Seleccione una Opcion$',13,10
 
 ;***********************************************************************
 
