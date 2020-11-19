ac8a097e40bf2ee32bfab4b74d06e759a7bf5058
Rebecca Medina <rebemedi99@gmail.com>
2020-11-19 00:32:20 -0600

    A mimir

--- Calculadora1_Grupo1/elemento.asm
+++ Calculadora1_Grupo1/elemento.asm
@@ -4,53 +4,77 @@
 ; FECHA: 29/10/2015
 ; Holis solo estoy probando
 
+INCLUDE c:\Irvine\Irvine32.inc
+INCLUDELIB c:\Irvine\Irvine32.lib
 
+		.listall
 
-Menu: .
-;esta es una etiqueta se llama menu que sera donde mostraremos las opciones al usuario
-     mov ah,0
-     mov al,3h ;modo texto
-     int 10h ; interrupcion de video
+		.data		;Directiva de Inicio de Variables. 
 
-     mov ax,0600h ;limpiar pantalla
-     mov bh,0fh ;0 color de fondo negro, f color de letra blanco
-     mov cx,0000h
-     mov dx,184Fh
-     int 10h
+Msg1	byte		"DADO EN ENSAMBLADOR REBECA: ",0
+Msg2	byte		"Numero : ",0
+Msg3	byte		"Tirar otra Vez (Y/N)? : ",0
 
-     mov ah,02h
-     mov bh,00
-     mov dh,00
-     mov dl,00
-     int 10h
-     mov dx, mostrarMenu ;nombre del mensaje
-     mov ah,09 ; mov ah,09 sirve para presentar el msj en pantalla
-     int 21h ;interrupcion de video
+Num		sdword		0		; Número Aleatorio.
 
-     mov ah,08 ;pausa y captura de datos 08 espera que el usuario presione una tecla
-     int 21h
+		.code
 
-     cmp al,49 ; ascii 49 = numero 1 compara lo que tiene el registro ah con el ascii 49 en el reg al
-     je op1 ; salto condicional jump equals opcion 1 saltar si es igual a la opcion 1
+;***********************************************************************
+; Función MAIN
+;***********************************************************************
+		
+main	PROC
 
-     cmp al,50 ;compara con opcion 2 apellido
-     je op2
+; Inicializa Generador Aleatorio ***************************************
 
-     cmp al,51 ;compara con opcion 3 carne
-     je op3
+		call	Randomize
 
-     cmp al,52 ;compara con opcion 4 salir
-     je op4
+; Despliega Mensaje de Inicial *****************************************
 
-     mostrarMenu db 'Universidad San Carlos de Guatemala',13,10 ; 13 es un 'enter' 10 un espacio
-                 db 'Facultad de Ingenieria',13,10
-                 db 'Escuela de Ciencias y Sistemas',13,10
-                 db 'Arquitectura de Computadoras y Ensambladores 1',13,10,13,10
-                 db '-.-.MENU.-.-',13,10,13,10
-                 db '1. Ver Nombre',13,10
-                 db '2. Ver Apellido',13,10
-                 db '3. Ver Carne',13,10
-                 db '4. Salir',13,10,13,10
-                 db 'Seleccione una Opcion$',13,10
+Inicio:	mov		edx, OFFSET Msg1	; EDX apunta a inicio de Msg1.
+		call	WriteString
+		call	Crlf
+		call	CrlF
+
+; Proceso de Tirar el Dado *********************************************
+
+Dado:	mov		edx, OFFSET Msg2	; Escribe Número.
+		call	WriteString
+					
+		call	RanPro				; Genera Número Aleatorio en Tema
+		mov		eax,Num
+		call	WriteDec
+		call	Crlf
+		call	Crlf
+
+		mov		edx, OFFSET Msg3	; Despliega Mensaje de Continuar.
+		call	WriteString
+
+		call	ReadChar			; Revisa si Continúa.
+		cmp		al,'y'
+		jne		Final
+		call	Clrscr
+		jmp		Inicio
+
+Final:	exit
+
+main	ENDP
+
+;***********************************************************************
+; Cálculo del Número Aleatorio entre 1 y 6
+;***********************************************************************
+
+RanPro	PROC
+
+		mov		eax,6			; Rango Aleatorio de 0 a 5.
+		call	RandomRange
+		inc		eax				; Pasa Rango Aleatorio de 1 a 6.
+		cmp		eax,Num			; Compara si Número no igual a anterior.
+		je		RanPro
+		mov		Num,eax
+
+		RET
+		
+RanPro ENDP
 
 END
